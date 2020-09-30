0eb5d1706554c82a8a1991d168e8f088c61c8c6b
natachasha <kafffkins@yahoo.co.uk>
2020-09-30 18:04:01 +0300

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,12 @@
 // +++++++++++++++++++++++++++++ About Slider ++++++++++++++++++++++++++
 var $status = $(".pageinfo");
 var $slickElement = $(".about-slider");
-$slickElement.on("init reInit afterChange", function (event, slick, currentSlide, nextSlide) {
+$slickElement.on("init reInit beforeChange", function (event, slick, currentSlide, nextSlide) {
 	//currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
 	// if (!slick.$dots) {
 	// 	return;
 	// }
+
 	var i = (currentSlide ? currentSlide : 0) + 1;
 	$status.text(i);
 });
@@ -36,3 +37,45 @@ const sliderAboutObserver = new IntersectionObserver(function (entries, sliderAb
 }, sliderAboutOptions);
 
 sliderAboutObserver.observe(sliderAbout);
+// +++++++++++++++++++++++++++++++ Project Slider ++++++++++++++++++++++++++++++++++++
+$(".projects-slider").slick({
+	arrows: true,
+	autoplay: true,
+
+	dots: false,
+	easing: "ease-in-out",
+	infinite: true,
+	pauseOnHover: false,
+	slidesToShow: 3,
+	slidesToScroll: 1,
+	// responsive: [
+	// {
+	// 	breakpoint: 1024,
+	// 	settings: {
+	// 		slidesToShow: 3,
+	// 		infinite: true,
+	// 	},
+	// },
+	// {
+	// 	breakpoint: 600,
+	// 	settings: {
+	// 		slidesToShow: 2,
+	// 		dots: true,
+	// 	},
+	// },
+	// 	{
+	// 		breakpoint: 1024,
+	// 		settings: "unslick", // destroys slick
+	// 	},
+	// ],
+});
+// +++++++++++++++++++++++++++++++ Footer Slider ++++++++++++++++++++++++++++++++++++
+$(".partners-slider").slick({
+	arrows: false,
+	autoplay: true,
+	dots: false,
+	infinite: true,
+	pauseOnHover: false,
+	slidesToShow: 5,
+	slidesToScroll: 2,
+});
